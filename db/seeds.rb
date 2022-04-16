47647615611d83a33e0303b8f6ade8be2a629729
Kelsi Olstad <kelsiolstad@gmail.com>
2022-04-16 16:45:43 -0600

    seed posts data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# Helper functions
+def open_asset(file_name)
+  File.open(Rails.root.join('db', 'seed_assets', file_name))
+end
+
 User.create!([{
   name: "Miranda",
   email: "miranda@fake.com",
@@ -50,3 +55,53 @@ level: 2
  }]) 
 
  puts "User.count", User.count
+
+ Post.create!([
+   {
+  text: "My latest work of art! Meet Mr. Elephant.",
+  image: open_asset("post_1.jpg"),
+  user_id: 1
+ },
+ {
+  text: "LOL",
+  image: open_asset("post_2.jpg"),
+  user_id: 2
+ },
+ {
+  text: "Morning breakfast!☀️🍓🍓🥞",
+  image: open_asset("post_3.jpg"),
+  user_id: 3
+ },
+ {
+  text: "Brothers and sisters. Miranda Priestly.",
+  image: open_asset("post_4.jpg"),
+  user_id: 4
+ },
+ {
+  text: "Fuck coffeecup",
+  image: open_asset("post_5.jpg"),
+  user_id: 5
+ },
+ {
+  text: "😜",
+  image: open_asset("post_6.jpg"),
+  user_id: 6
+ },
+ {
+  text: "Becoming a photographer with my 📸",
+  image: open_asset("post_7.jpg"),
+  user_id: 6
+ },
+ {
+  text: "Mom and dad's anniversary🍷",
+  image: open_asset("post_8.jpg"),
+  user_id: 3
+ },
+ {
+  text: "My new best friend",
+  image: open_asset("post_9.jpg"),
+  user_id: 2
+ }
+])
+
+puts "Post.count", Post.count
