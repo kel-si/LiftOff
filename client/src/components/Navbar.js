811a55a6fa214a790137b8d09fcf7a247873e6a3
Kelsi Olstad <kelsiolstad@gmail.com>
2022-04-19 12:06:53 -0600

    add basic fillers in the nav bar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,16 +1,22 @@
 import React from "react";
+import "../styles/components.css";
 
 export default function Navbar() {
   return (
-    <div>
-      <p>My Posts Button</p>
-      <img
-        className="avatar"
-        src="https://i.pinimg.com/originals/f3/8f/15/f38f150e6ae908a1e908597a92eb1e99.png"
-      />
-      <p>Level 1</p>
-      <p>Username</p>
-      <p>Log Out Button</p>
-    </div>
+    <nav>
+      <h1>Logo</h1>
+      <ul>
+        <li>My Posts Button</li>
+        <li>Guidelines</li>
+        <li>Log Out Button</li>
+      </ul>
+      <container className="avatar-container">
+        <img
+          className="avatar-image"
+          src="https://i.pinimg.com/originals/f3/8f/15/f38f150e6ae908a1e908597a92eb1e99.png"
+        />
+        <p>Level 1</p>
+      </container>
+    </nav>
   );
 }
