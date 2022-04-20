8f2c083b5b1eff7b21edff545435cd3cb0bf6418
Kelsi Olstad <kelsiolstad@gmail.com>
2022-04-20 14:46:04 -0600

    add authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ApplicationRecord
     has_many :posts
     has_many :comments
+
+    def self.authenticate_with_credentials email, password
+        user = User.find_by_email(email)
+        if user
+          if user.authenticate password
+          user
+          end
+        else
+          nil
+        end
+      end
 end
