55b34bf8626e3b312ea3e034433bf0cad2772500
Kelsi Olstad <kelsiolstad@gmail.com>
2022-04-20 11:56:01 -0600

    create Landing component

---
+++ client/src/components/Landing.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Login from "./user-management/Login";
+
+export default function Landing() {
+  return (
+    <div>
+      <h1>Landing Page</h1>
+      <Login />
+    </div>
+  );
+}
