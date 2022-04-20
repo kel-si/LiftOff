0bddecfcf9186b229e2dcfb9319750798b02294b
rkrowchuk <rhiannonkrowchuk@gmail.com>
2022-04-20 09:43:17 -0700

    update post form

--- client/src/components/CreatePost.js
+++ client/src/components/CreatePost.js
@@ -2,40 +2,24 @@ import React, { useState, useEffect }  from 'react'
 
 export default function CreatePost( ) {
 
-  const [post, setPost] = useState({
-    description: "",
-  });
-  //remember to edit: add image back to post, setPost
-  const [image, setImage] = useState({
-    image: "",
-  });
-  // const handleSubmit= (e) => {
-    // makeNewPost([post, image])
-    // e.preventDefault();
-  // }
+  const [post, setPost] = useState("");
+  const [image, setImage] = useState("");
+  
   const handleSubmit =(e) => {
     e.preventDefault();
-    console.log(e)
-    console.log(post);
+    console.log("post:", post, "image:", image);
    }
-
-   const handleChange = (e) => {
-     setPost({value: e.target.value})
-   }
-
-   console.log(post);
-  console.log(post.description);
 
   return (
     <div className="form-container">
-      <form onSubmit={e => { handleSubmit(e)} }>
+      <form onSubmit={ handleSubmit } >
         <div>
           <input
             type="text"
             name="description"
             placeholder="description"
-            value={post.description}
-            onChange={handleChange}
+            value={ post }
+            onChange={(event) => setPost(event.target.value)}
           />
         </div>
         <div>
@@ -43,18 +27,18 @@ export default function CreatePost( ) {
             type="file"
             name="image"
             placeholder="image"
-            value={post.image}
-            onChange={handleChange}
+            value={ image }
+            onChange={(event) => setImage(event.target.value)}
           />
         </div>
         <div>
           <button type="submit" value="Add Post">Post</button>
         </div>
-        <p>{post.description}</p>
+  
       </form>
 
       <div>
-        <p>{post.description}</p>
+
       </div>
     </div>
   );
