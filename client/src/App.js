f2aead8e4a061bc161f53220388c3976d9498c17
Kelsi Olstad <kelsiolstad@gmail.com>
2022-04-19 18:58:37 -0600

    clean up directory

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "./App.css";
 import PostList from "./components/PostList";
 import Navbar from "./components/Navbar";
 
