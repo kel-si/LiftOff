4d7291694326025d6621179f8777343bbdf1eae9
Kelsi Olstad <kelsiolstad@gmail.com>
2022-04-20 11:55:35 -0600

    import Landing page and render instead of Feed

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,18 @@ import Navbar from "./components/Navbar";
 import Feed from "./components/Feed";
 import MyPosts from "./components/MyPosts";
 import Guidelines from "./components/Guidelines";
+import Landing from "./components/Landing";
 
 export default function App() {
-
   return (
     <div className="App">
       <Router>
-         <Navbar />
-         <Routes>
-           <Route path="/my-posts" element={<MyPosts />}/>
-           <Route path="/guidelines" element={<Guidelines />}/>
-           <Route path="/" element={<Feed />}/>
-         </Routes>
+        <Navbar />
+        <Routes>
+          <Route path="/my-posts" element={<MyPosts />} />
+          <Route path="/guidelines" element={<Guidelines />} />
+          <Route path="/" element={<Landing />} />
+        </Routes>
       </Router>
     </div>
   );
