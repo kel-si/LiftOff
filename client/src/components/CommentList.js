f9c0284276eb2422229f0f889b440fec625e7b97
Kelsi Olstad <kelsiolstad@gmail.com>
2022-04-20 20:49:41 -0600

    add CommentList function in component

---
+++ client/src/components/CommentList.js
@@ -0,0 +1,10 @@
+import React from "react";
+import CommentListItem from "./CommentListItem";
+
+export default function CommentList() {
+  return (
+    <div>
+      <CommentListItem />
+    </div>
+  );
+}
