0ebf5fc5cfd171a9f99bf193ed7266e89bcf3f06
Kelsi Olstad <kelsiolstad@gmail.com>
2022-04-20 20:51:06 -0600

    remove console.log and commented out code

--- client/src/components/PostListItem.js
+++ client/src/components/PostListItem.js
@@ -1,12 +1,10 @@
 import React from "react";
 
 export default function PostListItem(props) {
-  console.log(typeof props.image);
   return (
     <div className="post--container">
       <article className="post">
         <header className="post--header">
-          {/* <img className="post--avatar" src={post.avatar} /> */}
           <h2 className="post--name">{props.name}</h2>
         </header>
 
@@ -14,11 +12,6 @@ export default function PostListItem(props) {
           <p>{props.text}</p>
           <img src={props.image} alt="error" />
         </div>
-
-        {/* <div className="post--image">
-          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/The_Earth_seen_from_Apollo_17.jpg/1280px-The_Earth_seen_from_Apollo_17.jpg" />
-        </div> */}
-
         <footer className="post--footer">
           <small className="footer--age">{props.time}</small>
         </footer>
