1d05bcbc0573c116440724f4ae4f437676bac3e9
rkrowchuk <rhiannonkrowchuk@gmail.com>
2022-04-21 14:12:30 -0700

    adding status columns to posts and comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_16_194906) do
+ActiveRecord::Schema.define(version: 2022_04_21_203917) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2022_04_16_194906) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.bigint "post_id"
+    t.integer "status", default: 0
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
@@ -31,6 +32,7 @@ ActiveRecord::Schema.define(version: 2022_04_16_194906) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.integer "status", default: 0
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -39,7 +41,7 @@ ActiveRecord::Schema.define(version: 2022_04_16_194906) do
     t.string "email"
     t.string "password_digest"
     t.string "parent_email"
-    t.integer "level"
+    t.integer "level", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
