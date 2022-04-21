8d09a51a0ad7aa3ed50668309fb0512a2230efa9
Kelsi Olstad <kelsiolstad@gmail.com>
2022-04-20 20:50:39 -0600

    call CommentList to test

--- client/src/components/MyPosts.js
+++ client/src/components/MyPosts.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Feed from "./Feed";
-
+import CommentList from "./CommentList";
 export default function MyPosts(props) {
   return (
     <div>
       <Feed />
+      <CommentList />
     </div>
   );
 }
