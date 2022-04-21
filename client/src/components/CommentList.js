ee12bb8261e0ddeea858406d481c29df39afd39a
Kelsi Olstad <kelsiolstad@gmail.com>
2022-04-20 22:58:00 -0600

    pass props; map comments for CommentListItem

--- client/src/components/CommentList.js
+++ client/src/components/CommentList.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CommentListItem from "./CommentListItem";
+import axios from "axios";
 
-export default function CommentList() {
-  return (
-    <div>
-      <CommentListItem />
-    </div>
-  );
+export default function CommentList(props) {
+  console.log("listitemprops", props.comments);
+  const comments = props.comments.map((comment) => {
+    return (
+      <div>
+        <CommentListItem
+          key={comment.id}
+          text={comment.text}
+          time={comment.created_at}
+        />
+      </div>
+    );
+  });
+  return <div>{comments}</div>;
 }
