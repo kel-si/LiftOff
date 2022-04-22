6b6abe5db21421e2c291d279eb3d06ccfed2455c
Kelsi Olstad <kelsiolstad@gmail.com>
2022-04-21 20:35:41 -0600

    change logo to liftoff

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,27 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../styles/components.css";
+import "../styles/components.scss";
 
 export default function Navbar(props) {
   return (
     <nav>
-      <h1><Link to="/">Logo</Link></h1>
+      <h1>
+        <Link to="/">LiftOff🚀</Link>
+      </h1>
       <ul>
         <button>
-          <li><Link to="/my-posts">My Posts Button</Link></li>
+          <li>
+            <Link to="/my-posts">My Posts Button</Link>
+          </li>
         </button>
         <button>
-        <li><Link to="/guidelines">Guidelines</Link></li>
+          <li>
+            <Link to="/guidelines">Guidelines</Link>
+          </li>
         </button>
         <button onClick={props.logout}>
-          <li><Link to="/">Log Out Button</Link></li>
+          <li>
+            <Link to="/">Log Out Button</Link>
+          </li>
         </button>
       </ul>
       <div className="avatar-container">
         <img
           className="avatar-image"
           src="https://i.pinimg.com/originals/f3/8f/15/f38f150e6ae908a1e908597a92eb1e99.png"
-        alt="shiba in the universe" />
+          alt="shiba in the universe"
+        />
         <p>Level 1</p>
       </div>
     </nav>
