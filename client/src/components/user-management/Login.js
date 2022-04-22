36a598d9f8151017b17dde9bbe6fb904dedbaeb0
Kelsi Olstad <kelsiolstad@gmail.com>
2022-04-21 22:12:16 -0600

    remove console.log

--- client/src/components/user-management/Login.js
+++ client/src/components/user-management/Login.js
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 export default function Login(props) {
-
   const [formValue, setformValue] = useState({
     email: "",
     password: "",
@@ -17,51 +16,50 @@ export default function Login(props) {
 
   const navigate = useNavigate();
 
-  const handleSubmit = (event) => { 
+  const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("formValue", formValue);
     axios
       .post("/api/login", { formValue })
       .then((res) => {
-      if (res.data.logged_in) {
-        props.handleLogin(res.data)
-        console.log("handleSubmit Login", res.data)
-        navigate("/my-posts");
-      } else {
-        setformValue({
-          errors: res.data.errors 
-        })
-      }
+        if (res.data.logged_in) {
+          props.handleLogin(res.data);
+          console.log("handleSubmit Login", res.data);
+          navigate("/my-posts");
+        } else {
+          setformValue({
+            errors: res.data.errors,
+          });
+        }
       })
       .catch((err) => {
-        console.log('api errors:', err)
+        console.log("api errors:", err);
       });
   };
 
-
   // const handleErrors = () => {
-    // return (
-      // <div>
-        //{/* {/* <ul>{formValue.errors.map((error) => { */}
-          // // return <li key={error}>{error}</li>
-        // })}
-        //{/* </ul>  */}
-      //{/* </div> */}
-    // )
+  // return (
+  // <div>
+  //{/* {/* <ul>{formValue.errors.map((error) => { */}
+  // // return <li key={error}>{error}</li>
+  // })}
+  //{/* </ul>  */}
+  //{/* </div> */}
+  // )
   // };
-
 
   return (
     <form onSubmit={handleSubmit} className="form-container">
       <h2>Login to Get Started</h2>
-      <input className="form--input"
+      <input
+        className="form--input"
         type="email"
         name="email"
         placeholder="enter an email"
         value={formValue.email}
         onChange={handleChange}
       />
-      <input className="form--input"
+      <input
+        className="form--input"
         type="password"
         name="password"
         placeholder="enter a password"
