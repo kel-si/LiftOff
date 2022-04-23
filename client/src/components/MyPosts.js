8ed8a10d7c5913ee4a3c2959c76b40938f1a26de
rkrowchuk <rhiannonkrowchuk@gmail.com>
2022-04-22 20:51:32 -0700

    fix white screen error on my posts with redirect to landing if user is not logged in

--- client/src/components/MyPosts.js
+++ client/src/components/MyPosts.js
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Feed from "./Feed";
+import { useNavigate } from "react-router-dom";
+
+
 export default function MyPosts(props) {
+
+  const [user, setUser] = useState({});
+  const [login, setLogin] = useState(0);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const currentUser = localStorage.getItem("liftoffUser");
+    if (!currentUser) {
+      navigate("/");
+    } else {
+      const liftoffUser = JSON.parse(currentUser);
+      setUser(liftoffUser.user);
+      setLogin(1);
+    }
+  }, [navigate]);
+
+
   return (
     <div>
       <Feed />
