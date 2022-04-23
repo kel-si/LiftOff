2f3159677e734a9b06c72166a231f2bfb966c777
Kelsi Olstad <kelsiolstad@gmail.com>
2022-04-23 17:00:26 -0600

    create Confirm component

---
+++ client/src/components/Confim.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Confirm(props) {
+  return (
+    <div>
+      <h4>✋You are about to comment on someone's post</h4>
+      <ul>
+        How will it make the owner of this post feel?
+        <li>💛 Is it kind?</li>
+        <li>✅ Is it true?</li>
+        <li>🐝 Is it necessary?</li>
+      </ul>
+      <section>
+        <button onClick={props.onCancel}>Cancel</button>
+        <button onClick={props.onSubmit}>Comment</button>
+      </section>
+    </div>
+  );
+}
