dcd96e3a96a6959a5279f7bee4836302f7bc47cd
Kelsi Olstad <kelsiolstad@gmail.com>
2022-04-22 19:41:07 -0600

    remove puts and commented out code

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -10,20 +10,13 @@ class Api::CommentsController < ApplicationController
   end
 
   def create
-    # @user = User.create(comment_params[:user_id])
-    puts "params", params
-    puts "another one", params[:comment]
     @comment = Comment.new(comment_params)
-    puts "comment_params", comment_params
-    # @user = comment_params[:user_id]
-    # @comment.user = @user
-    @comment.post_id = 1
+
     if @comment.save 
       render json: {
         comment: @comment 
     }
     else 
-      puts @comment.errors.inspect
       render json: {
       errors: ["not getting it"]
       }
@@ -32,6 +25,6 @@ class Api::CommentsController < ApplicationController
 
   private 
   def comment_params 
-    params.require(:comment).permit(:text, :user_id)
+    params.require(:comment).permit(:text, :user_id, :post_id)
   end 
 end
