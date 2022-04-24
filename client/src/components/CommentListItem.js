df332e0f74ed9e586830313334032d67dc10fcea
JLMaynardDesign <JLMaynardDesign@gmail.com>
2022-04-24 14:43:20 -0400

    add conditional rendering of comments, based on approval status - show pending or not

--- client/src/components/CommentListItem.js
+++ client/src/components/CommentListItem.js
@@ -5,15 +5,34 @@ import "../styles/Comments.scss";
 export default function CommentListItem(props) {
 
   const time = Moment(props.time).startOf('hour').fromNow();
-
+  const approval = props.status;
   return (
-    <div className="comment--container">
+    <>
+    { approval === 0 ? (
+<div className="comment--container">
+  <article className="comment">
+    <header className="comment--header">
+      <img src={props.avatar} className="comment-avatar"/>
+      <h6 className="comment--name">{props.name}</h6>
+    </header>
+    <div className="comment--body">
+      <div className="comment--body-text">
+        <p>{props.text}</p>
+      </div>
+      <footer className="comment--footer">
+      <small className="footer--age">{time}</small>
+      <small className="footer--age">pending</small>
+    </footer>
+    </div>
+  </article>
+</div>
+    ):(
+      <div className="comment--container">
       <article className="comment">
         <header className="comment--header">
           <img src={props.avatar} className="comment-avatar"/>
           <h6 className="comment--name">{props.name}</h6>
         </header>
-
         <div className="comment--body">
           <div className="comment--body-text">
             <p>{props.text}</p>
@@ -25,5 +44,7 @@ export default function CommentListItem(props) {
         
       </article>
     </div>
+    )}
+    </>
   );
 }
