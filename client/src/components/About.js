de335ac3039fa4db6b7dbff7b9fbe380b38d00d8
Kelsi Olstad <kelsiolstad@gmail.com>
2022-04-25 12:06:00 -0600

    add text blob

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,5 +1,41 @@
 import React from "react";
 
 export default function About() {
-  return <div>About!!!</div>;
+  return (
+    <div>
+      <h1>About LiftOff</h1>
+      <div>
+        Get ready for LiftOff! LiftOff is a social media app designed for
+        children, between the ages of 8-10 years old, meant to introduce users
+        to the world of social media! LiftOff was created as a stepping stone
+        for children to learn fundamental digital citizenship skills, such as
+        safety, respect, and critical thinking, in order to partake in social
+        media responsibly. At LiftOff, we encourage our users to participate
+        with intention. We want users to be aware, and in control of, how much
+        time they are spending on social media, as well as how their actions
+        online impact others. We hope that once users are ready for other social
+        media sites, they will be prepared to participate appropriately wherever
+        they go. In order to gain these skills, users will move through the
+        following levels: Earthling Immediately after registering, earthlings
+        will be prompted to complete a quiz to unlock the app. Users will need
+        to select the most appropriate comment from a list based on the photo.
+        This is to get earthlings thinking about appropriate comments! Mini
+        Martian Upon registering, users will begin with limited capabilities as
+        a Mini Martian. Mini Martians are able to view posts created by other
+        users, but will not be able to post or freely comment just yet. As a
+        Mini Martian, the goal is to identify appropriate comments - both from
+        others, as well as how to comment appropriately. We want users to ask
+        themselves why they are commenting. Is the comment kind? Is it true? Is
+        it necessary? Mini Martians will have the opportunity to create comments
+        which will be approved by the Supreme Leader administrator. Once an
+        earthling creates 10 approved comments, they will level up to “Moon
+        Walker” Moon Walker Moon Walkers have full commenting privileges! They
+        also have the ability to create a post, which will need to be approved
+        by the Supreme Leader administrator. Moon Walkers have more freedom on
+        the app; however, if Moon Walkers are found to not be following the
+        community guidelines, they may be leveled down to a Mini Martian. Are
+        you ready? T-Minus 3, 2, 1… LiftOff!
+      </div>
+    </div>
+  );
 }
