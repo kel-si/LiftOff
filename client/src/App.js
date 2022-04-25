9d67d61cf8b7f710a2365444c82a886d0f669277
Kelsi Olstad <kelsiolstad@gmail.com>
2022-04-25 12:03:42 -0600

    make route to the about page in the nav bar and call the component

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Guidelines from "./components/Guidelines";
 import Landing from "./components/Landing";
 import Register from "./components/user-management/Register";
 import Quiz from "./components/Quiz";
+import About from "./components/About";
 import axios from "axios";
 import AdminLanding from "./components/admin/AdminLanding";
 import AdminFeed from "./components/admin/AdminFeed";
@@ -15,7 +16,7 @@ import AdminUsers from "./components/admin/AdminUsers";
 export default function App() {
   const [login, setLogin] = useState({
     status: false,
-    user: {}
+    user: {},
   });
 
   const handleLogin = (data) => {
@@ -47,31 +48,49 @@ export default function App() {
   };
 
   const handleQuizCompletion = (level) => {
-    const newUserState = {...login.user, level: level}
-    setLogin(prev => ({...prev, user: newUserState}));
-  }
+    const newUserState = { ...login.user, level: level };
+    setLogin((prev) => ({ ...prev, user: newUserState }));
+  };
 
   useEffect(() => {
     loginStatus();
     console.log("login", login);
   }, []);
 
-
-
   return (
     <div className="App">
       <Router>
-        { login.status ? ( <> <Navbar user={login.user} setLogin={setLogin} logout={handleLogout}/> </>) : (<></>) }
+        {login.status ? (
+          <>
+            {" "}
+            <Navbar
+              user={login.user}
+              setLogin={setLogin}
+              logout={handleLogout}
+            />{" "}
+          </>
+        ) : (
+          <></>
+        )}
         <Routes>
           <Route path="/" element={<Landing handleLogin={handleLogin} />} />
-            <Route path="/login" element={<Login handleLogin={handleLogin} />} />
-            <Route path="/register" element={<Register handleLogin={handleLogin} />} />
-            <Route path="/my-posts" element={<Feed />} />
-            <Route path="/guidelines" element={<Guidelines />} />
-            <Route path="/quiz" element={<Quiz updateLevel={handleQuizCompletion} user={login.user}/>} />
-            <Route path="/admin" element={<AdminLanding />} />
-            <Route path="/admin-approvals" element={<AdminFeed />} />
-            <Route path="/admin-users" element={<AdminUsers />} />
+          <Route path="/login" element={<Login handleLogin={handleLogin} />} />
+          <Route
+            path="/register"
+            element={<Register handleLogin={handleLogin} />}
+          />
+          <Route path="/my-posts" element={<Feed />} />
+          <Route path="/guidelines" element={<Guidelines />} />
+          <Route path="/about" element={<About />} />
+          <Route
+            path="/quiz"
+            element={
+              <Quiz updateLevel={handleQuizCompletion} user={login.user} />
+            }
+          />
+          <Route path="/admin" element={<AdminLanding />} />
+          <Route path="/admin-approvals" element={<AdminFeed />} />
+          <Route path="/admin-users" element={<AdminUsers />} />
         </Routes>
       </Router>
     </div>
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -7,9 +7,8 @@ import { levelName, levelAvatar } from "./helpers/navHelpers";
 import { useTimer } from "use-timer";
 
 export default function Navbar(props) {
-  
   const { time, start, pause, reset, status } = useTimer({
-    interval: 60000
+    interval: 60000,
   });
 
   useEffect(() => {
@@ -17,8 +16,8 @@ export default function Navbar(props) {
   }, []);
 
   const logOut = () => {
-  axios
-    .post("/api/logout", { withCredentials: true }) // sends credentials to server
+    axios
+      .post("/api/logout", { withCredentials: true }) // sends credentials to server
       .then((response) => {
         console.log("response:", response.data);
         localStorage.clear();
@@ -26,46 +25,67 @@ export default function Navbar(props) {
       })
       .catch((err) => {
         console.log(err);
-      })
+      });
   };
 
   return (
     <>
-    <div className="timer-container">
-      { time === 1 ? (<p>You've been browsing for {time} minute </p>) : ( <p>You've been browsing for {time} minutes</p> ) }
-    {status === 'RUNNING'}
-    <ul className="nav-menu">
-          <li><Link to="/my-posts">My Posts</Link></li>
-          <li><Link to="/guidelines">Guidelines</Link></li>
-        {props.user.level > 2 ? (<li><Link to="/admin">Admin</Link></li>) : (<></>)}
-      </ul>
-    </div>
-      
-    <nav>
-      <h1 className="logo">
-        <Link to="/">
-          LiftOff
-          <span role="img" aria-label="rocket ship emoji">
-            🚀
-          </span>
-        </Link>
-      </h1>
-
-      <div className="avatar-container">
-        <div className="avatar-left">
-        <img
-          className="avatar-image"
-          src={levelAvatar(props.user.level)}
-          alt="avatar"
-        />
-        </div>
-            <div className="avatar-right">
-                <h3>{props.user.name}</h3>
-                <p>Level: {levelName(props.user.level)}</p>
-                <p><Link to="/" onClick={logOut}>Log Out</Link></p>
-            </div>
+      <div className="timer-container">
+        {time === 1 ? (
+          <p>You've been browsing for {time} minute </p>
+        ) : (
+          <p>You've been browsing for {time} minutes</p>
+        )}
+        {status === "RUNNING"}
+        <ul className="nav-menu">
+          <li>
+            <Link to="/my-posts">My Posts</Link>
+          </li>
+          <li>
+            <Link to="/guidelines">Guidelines</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          {props.user.level > 2 ? (
+            <li>
+              <Link to="/admin">Admin</Link>
+            </li>
+          ) : (
+            <></>
+          )}
+        </ul>
       </div>
-    </nav>
+
+      <nav>
+        <h1 className="logo">
+          <Link to="/">
+            LiftOff
+            <span role="img" aria-label="rocket ship emoji">
+              🚀
+            </span>
+          </Link>
+        </h1>
+
+        <div className="avatar-container">
+          <div className="avatar-left">
+            <img
+              className="avatar-image"
+              src={levelAvatar(props.user.level)}
+              alt="avatar"
+            />
+          </div>
+          <div className="avatar-right">
+            <h3>{props.user.name}</h3>
+            <p>Level: {levelName(props.user.level)}</p>
+            <p>
+              <Link to="/" onClick={logOut}>
+                Log Out
+              </Link>
+            </p>
+          </div>
+        </div>
+      </nav>
     </>
   );
 }
