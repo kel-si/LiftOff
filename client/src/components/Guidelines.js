ae52866e6eda3a0ef9e87be1de3778c4baa054de
Kelsi Olstad <kelsiolstad@gmail.com>
2022-04-25 11:43:56 -0600

    first draft of guidelines

--- client/src/components/Guidelines.js
+++ client/src/components/Guidelines.js
@@ -2,6 +2,61 @@ import React from "react";
 
 export default function Guidelines(props) {
   return (
-    <div><h1>Guidelines</h1></div>
-  )
+    <div>
+      <h1 className="text--title">Guidelines</h1>
+      <div className="text--info">
+        <h2 className="text--title">Safety First!</h2>
+        <ul>
+          Never share any personal information. Information that should be kept
+          private are things like:
+        </ul>
+        <li>Your full name</li>
+        <li>Where you live</li>
+        <li>Where you go to school</li>
+        <li>Your email address</li>
+        <li>Your password</li>
+        <li>Your phone number</li>
+
+        <ul>
+          When you are posting a photo make sure you don't include any private
+          information by accident like:
+        </ul>
+        <li>Your school or school uniform</li>
+        <li>Your house or your street</li>
+        <li>
+          Post a photo of a place after you leave (you don't want to show your
+          current location!)
+        </li>
+        <p>
+          Make sure to communicate with a parent or guardian about your social
+          media use! If something is making you feel uncomfortable, tell a
+          trusted adult right away!
+        </p>
+        <p>
+          Remember, anything you put on social media is available for anyone to
+          see!
+        </p>
+
+        <h2 className="text--title">Be Respectful</h2>
+        <ul>When you post or commennt, make sure it is respectful!</ul>
+        <li>
+          Is it kind? Think about how your post or comment might make others
+          feel. How would this post or comment make you feel?
+        </li>
+        <li>Is it true? Posting things that we know are false is not okay.</li>
+        <li>
+          Is it necessary? We don't want to spam our fellow Mini Martians and
+          Moon Walkers with comments or posts. Commenting because we want to
+          share kind words, or share something related is awesome!
+        </li>
+        <h2 className="text--title">Be Mindful</h2>
+        <ul>Think about how you are using social media!</ul>
+        <li>
+          How much time are you on your phone, tablet, or computer? Set a
+          timelimit for yourself and stick to it!
+        </li>
+        <li>Think about why you are posting or commenting.</li>
+      </div>
+    </div>
+  );
 }
