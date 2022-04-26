8aee7e6d9ae5960c8dc3874181c5c66b65ee657f
Kelsi Olstad <kelsiolstad@gmail.com>
2022-04-26 13:23:04 -0600

    re-add style changes

--- client/src/components/user-management/Register.js
+++ client/src/components/user-management/Register.js
@@ -10,8 +10,7 @@ export default function Register(props) {
     password_confirmation: "",
     parent_email: "",
   });
-  const navigate = useNavigate(); 
-
+  const navigate = useNavigate();
 
   // sets state with the form values
   const handleChange = (event) => {
@@ -24,24 +23,24 @@ export default function Register(props) {
   // sends form into to server to create user
   const handleSubmit = (event) => {
     event.preventDefault();
-      axios
+    axios
       .post("/api/users", { user })
-        .then((res) => {
-          console.log("server responding to new user", res.data)
-          if (res.data.logged_in) {
-            props.handleLogin(res.data);
-            localStorage.setItem('liftoffUser', JSON.stringify(res.data));
-            navigate("/quiz");
-          } else {
-            setUser({
-              errors: res.data.errors,
-            });
-            }
-          })
-        // })
-        .catch((err) => {
-          console.log("api errors:", err);
-        });
+      .then((res) => {
+        console.log("server responding to new user", res.data);
+        if (res.data.logged_in) {
+          props.handleLogin(res.data);
+          localStorage.setItem("liftoffUser", JSON.stringify(res.data));
+          navigate("/quiz");
+        } else {
+          setUser({
+            errors: res.data.errors,
+          });
+        }
+      })
+      // })
+      .catch((err) => {
+        console.log("api errors:", err);
+      });
   };
 
   return (
@@ -52,54 +51,62 @@ export default function Register(props) {
           <span role="img" aria-label="rocket ship emoji">
             🚀
           </span>
-      </h1>
-      <form onSubmit={handleSubmit} className="form-container" autoComplete="off">
-        <h2>Register to Get Started</h2>
-        <input
-          className="form--input"
-          type="name"
-          name="name"
-          placeholder="Choose a username"
-          value={user.name || ""}
-          onChange={handleChange}
-        />
-        <input
-          className="form--input"
-          type="email"
-          name="email"
-          placeholder="enter an email"
-          value={user.email || ""}
-          onChange={handleChange}
-        />
-        <input
-          className="form--input"
-          type="password"
-          name="password"
-          placeholder="enter a password"
-          value={user.password || ""}
-          onChange={handleChange}
-        />
-        <input
-          className="form--input"
-          type="password"
-          name="password_confirmation"
-          placeholder="confirm your password"
-          value={user.password_confirmation || ""}
-          onChange={handleChange}
-        />
-        <input
-          className="form--input"
-          type="email"
-          name="parent_email"
-          placeholder="enter your parent's email"
-          value={user.parent_email || ""}
-          onChange={handleChange}
-        />
-        <button color="primary" type="submit" className="primary--btn">
-          Register
-        </button>
-      </form>
-      <Link to="/login">Login with your existing account</Link>
+        </h1>
+        <form
+          onSubmit={handleSubmit}
+          className="form-container"
+          autoComplete="off"
+        >
+          <h2>Register to Get Started</h2>
+          <input
+            className="form--input"
+            type="name"
+            name="name"
+            placeholder="choose a username"
+            value={user.name || ""}
+            onChange={handleChange}
+          />
+          <input
+            className="form--input"
+            type="email"
+            name="email"
+            placeholder="enter an email"
+            value={user.email || ""}
+            onChange={handleChange}
+          />
+          <input
+            className="form--input"
+            type="password"
+            name="password"
+            placeholder="enter a password"
+            value={user.password || ""}
+            onChange={handleChange}
+          />
+          <input
+            className="form--input"
+            type="password"
+            name="password_confirmation"
+            placeholder="confirm your password"
+            value={user.password_confirmation || ""}
+            onChange={handleChange}
+          />
+          <input
+            className="form--input"
+            type="email"
+            name="parent_email"
+            placeholder="enter your parent's email"
+            value={user.parent_email || ""}
+            onChange={handleChange}
+          />
+          <aside>
+            *make sure to get your parent or guardian's permission before
+            creating your account!
+          </aside>
+          <button color="primary" type="submit" className="primary--btn">
+            Register
+          </button>
+        </form>
+        <Link to="/login">Login with your existing account</Link>
       </div>
     </div>
   );
