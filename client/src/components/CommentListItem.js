344e5d6e61072400fac77cf0473a0e6b2c8f9926
Kelsi Olstad <kelsiolstad@gmail.com>
2022-04-25 22:18:20 -0600

    change classNames to target styles

--- client/src/components/CommentListItem.js
+++ client/src/components/CommentListItem.js
@@ -1,52 +1,50 @@
 import React from "react";
-import Moment from 'moment';
+import Moment from "moment";
 import "../styles/Comments.scss";
 
 export default function CommentListItem(props) {
-
   // defines the timestamp formatter
-  const time = Moment(props.time).startOf('hour').fromNow();
+  const time = Moment(props.time).startOf("hour").fromNow();
   const approval = props.status;
 
   return (
     <>
-    { approval === 0 ? (
-<div className="comment--container">
-  <article className="comment">
-    <header className="comment--header">
-      <img src={props.avatar} className="comment-avatar"/>
-      <h6 className="comment--name">{props.name}</h6>
-    </header>
-    <div className="comment--body">
-      <div className="comment--body-text">
-        <p>{props.text}</p>
-      </div>
-      <footer className="comment--footer">
-      <small className="footer--age">{time}</small>
-      <small className="footer--age">pending</small>
-    </footer>
-    </div>
-  </article>
-</div>
-    ):(
-      <div className="comment--container">
-      <article className="comment">
-        <header className="comment--header">
-          <img src={props.avatar} className="comment-avatar"/>
-          <h6 className="comment--name">{props.name}</h6>
-        </header>
-        <div className="comment--body">
-          <div className="comment--body-text">
-            <p>{props.text}</p>
-          </div>
-          <footer className="comment--footer">
-          <small className="footer--age">{time}</small>
-        </footer>
+      {approval === 0 ? (
+        <div className="comment--container">
+          <article className="comment">
+            <header className="comment--header">
+              <img src={props.avatar} className="comment-avatar" />
+              <h6 className="comment--name">{props.name}</h6>
+            </header>
+            <div className="comment--body">
+              <div className="comment--body-text">
+                <p>{props.text}</p>
+              </div>
+              <footer className="comment--footer">
+                <small className="footer--time">{time}</small> <br />
+                <small className="footer--pending">pending</small>
+              </footer>
+            </div>
+          </article>
         </div>
-        
-      </article>
-    </div>
-    )}
+      ) : (
+        <div className="comment--container">
+          <article className="comment">
+            <header className="comment--header">
+              <img src={props.avatar} className="comment-avatar" />
+              <h6 className="comment--name">{props.name}</h6>
+            </header>
+            <div className="comment--body">
+              <div className="comment--body-text">
+                <p>{props.text}</p>
+              </div>
+              <footer className="comment--footer">
+                <small className="footer--age">{time}</small>
+              </footer>
+            </div>
+          </article>
+        </div>
+      )}
     </>
   );
 }
