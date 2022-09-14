0942b947fbfac7c9eed7129b6da936cf8aa2158a
Kelsi Olstad <kelsiolstad@gmail.com>
2022-09-14 16:55:40 -0600

    check if user is logged in to show either the feed or the log in page on /

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,12 +1,19 @@
 import React from "react";
 import Login from "./user-management/Login";
+import Feed from "./Feed";
 import { Link } from "react-router-dom";
 
 export default function Landing(props) {
-
+  if (props.user.status) {
+    return (
+      <div className="page-container">
+        <Feed user={props.user} />
+      </div>
+    );
+  }
   return (
     <div className="page-container">
-      <Login handleLogin={props.handleLogin}/>
+      <Login handleLogin={props.handleLogin} />
     </div>
   );
 }
